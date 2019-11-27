c4f987227c86c2219ebd8e8ff390037eb829901f
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2019-11-27 09:15:15 -0300

    Bug fixes and adjustments.
    - The routine that checks if Redis is available has been changed. In the previous implementation, the nc command is not present on the container docker node. The socket is now used for verification;
    - Changed messages from publish on the FitbitDataRepository class bus;
    - Unused libraries removed.

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,6 @@ import { Default } from './utils/default'
 import { Identifier } from './di/identifiers'
 import { ILogger } from './utils/custom.logger'
 import { Strings } from './utils/strings'
-import ipAllowed from 'ip-allowed'
 import { DIContainer } from './di/di'
 
 /**
@@ -60,28 +59,12 @@ export class App {
      */
     private async initMiddleware(): Promise<void> {
         try {
-            await this.setupHostWhitelist()
             await this.setupInversifyExpress()
             this.setupSwaggerUI()
             this.setupErrorsHandler()
         } catch (err) {
             this._logger.error(`Fatal error in middleware configuration: ${(err && err.message) ? err.message : ''}`)
         }
-    }
-
-    /**
-     * Access control based on host addresses.
-     * Only allow requests from the hosts that are on the permissions list.
-     *
-     * @private
-     * @return Promise<void>
-     */
-    private async setupHostWhitelist(): Promise<void> {
-        this.express.use(ipAllowed(process.env.HOST_WHITELIST || Default.HOST_WHITELIST, {
-            log: (clientIp, accessDenied) => {
-                if (accessDenied) this._logger.warn(`Client with IP address ${clientIp} is not allowed!`)
-            }
-        }))
     }
 
     /**
