1ea32a230e93b71b41da05282a8cd599baa0c7e0
Douglas Rafael <douglas.rafael@nutes.uepb.edu.br>
2019-11-27 01:03:17 -0300

    Adjustments in task to sync Fitbit data.
    Added routine to wait for availability of a Redis instance before initializing queue settings.

--- src/background/task/sync.fitbit.data.task.ts
+++ src/background/task/sync.fitbit.data.task.ts
@@ -8,59 +8,103 @@ import cron from 'node-cron'
 import { IUserAuthDataService } from '../../application/port/user.auth.data.service.interface'
 import { Default } from '../../utils/default'
 import { DataSync } from '../../application/domain/model/data.sync'
-import { Job } from 'bull'
-import Bull = require('bull')
+import Bull, { Job } from 'bull'
+import url, { UrlWithStringQuery } from 'url'
+import shell from 'shelljs'
 
 @injectable()
 export class SyncFitbitDataTask implements IBackgroundTask {
     private schedule: any
-    private fitbitSyncQueue: Bull.Queue<UserAuthData>
-    private readonly queueOpts: Bull.JobOptions = {
-        removeOnComplete: true, // If true, removes the job when it successfully
-        removeOnFail: true // If true, removes the job when it fails after all attempts.
-    }
+    private syncQueue?: Bull.Queue<UserAuthData>
+    private redisURI: UrlWithStringQuery = url.parse(process.env.REDIS_URI || Default.REDIS_URI)
 
     constructor(
         @inject(Identifier.USER_AUTH_DATA_SERVICE) private readonly _userAuthDataService: IUserAuthDataService,
         @inject(Identifier.LOGGER) private readonly _logger: ILogger
     ) {
-        this.fitbitSyncQueue = new Bull('fitbit-sync', process.env.REDIS_URI || Default.REDIS_URI)
-        this.initListeners()
         this.schedule = cron.schedule(`${process.env.EXPRESSION_AUTO_SYNC || Default.EXPRESSION_AUTO_SYNC}`,
-            () => this.externalDataSync())
+            () => this.fitbitDataSync(), {
+                scheduled: false
+            })
     }
 
     public async run(): Promise<void> {
-        this.schedule.start()
-        // Process queue jobs
-        this.fitbitSyncQueue
-            .process(job => this._userAuthDataService.syncFitbitDataFromUser(job.data.user_id!))
-            .catch(e => this._logger.error(`An error occurred processing queue for Fitbit synchronization. ${e.message}`))
+        try {
+            if (this.syncQueue) {
+                await this.syncQueue.empty()
+                this.syncQueue = undefined
+                this.schedule.destroy()
+            }
+            // It is necessary wait until Redis is available to initialize the queue.
+            await this.waitRedis()
+
+            // Initialize Redis and register job processor
+            this.syncQueue = this.initRedis()
+            this.initRedisListeners()
+            this.syncQueue!
+                .process(job => this._userAuthDataService.syncFitbitDataFromUser(job.data.user_id!))
+                .catch(e => this._logger.error(`An error occurred processing queue for Fitbit synchronization. ${e.message}`))
+
+            // Initialize crontab
+            this.schedule.start()
+
+            this._logger.debug('Fitbit data synchronization task started successfully!')
+        } catch (e) {
+            this._logger.error(`An error occurred initializing the Fitbit data synchronization task. ${e.message}`)
+        }
     }
 
-    private externalDataSync(): void {
-        const query = new Query()
-        query.filters = { 'fitbit.status': 'valid_token' }
-
-        this._userAuthDataService
-            .getAll(query)
-            .then((usersData: Array<UserAuthData>) => {
-                if (!usersData.length) {
-                    this._logger.warn('There are no users with Fitbit access to perform synchronization...')
-                    return
-                }
-                // Create jobs and add them to queue to process later
-                usersData.forEach(item => this.fitbitSyncQueue.add(item, this.queueOpts))
-            })
-            .catch(err => {
-                this._logger.error(`An error occurred while trying to retrieve data for Fitbit sync. ${err.message}`)
-            })
+    public stop(): Promise<void> {
+        if (this.syncQueue) this.syncQueue!.empty().then()
+        this.schedule.destroy()
+        return Promise.resolve()
     }
 
-    private initListeners(): void {
-        this.fitbitSyncQueue
+    /**
+     * Function to block flow until Redis instance is available.
+     */
+    private waitRedis(): Promise<void> {
+        return new Promise<void>(resolve => {
+            const redisCheck = () => {
+                this._logger.warn(`Waiting for instance of Redis (${this.redisURI.host}) to become available`)
+                setTimeout(() => {
+                    const check = !(Number(shell.exec(`nc -vz -w 1 ${this.redisURI.hostname} ${this.redisURI.port}; echo $?`,
+                        { silent: true }).stdout))
+                    if (check) return resolve()
+                    redisCheck()
+                }, 2000)
+            }
+            redisCheck()
+        })
+    }
+
+    /**
+     * Initialize Redis configurations.
+     */
+    private initRedis(): Bull.Queue {
+        return new Bull('fitbit-sync', {
+            redis: {
+                port: Number(this.redisURI.port!),
+                host: this.redisURI.hostname!,
+                maxRetriesPerRequest: null,
+                enableReadyCheck: false
+            },
+            defaultJobOptions: {
+                removeOnComplete: true, // If true, removes the job when it successfully
+                removeOnFail: true // If true, removes the job when it fails after all attempts.
+            }
+        })
+    }
+
+    /**
+     * Initialize Redis events
+     */
+    private initRedisListeners(): void {
+        if (!this.syncQueue) return
+
+        this.syncQueue
             .on('completed', (job: Job, result: DataSync) => {
-                this._logger.info(`Fitbit data synchronization for child ${result.user_id} is finished!`)
+                this._logger.info(`Fitbit data sync for child ${result.user_id} is finished!`)
             })
             .on('failed', (job: Job, err: Error) => {
                 this._logger.error(
@@ -68,13 +112,29 @@ export class SyncFitbitDataTask implements IBackgroundTask {
                 )
             })
             .on('drained', () => {
-                this._logger.info('Fitbit data synchronization queue completed!')
+                this._logger.debug('All jobs in the Fitbit data sync queue have been executed!')
             })
     }
 
-    public stop(): Promise<void> {
-        this.fitbitSyncQueue.empty().then()
-        this.schedule.destroy()
-        return Promise.resolve()
+    /**
+     * Sync routine for external data like Fitbit.
+     */
+    private fitbitDataSync(): void {
+        const query = new Query()
+        query.filters = { 'fitbit.status': 'valid_token' }
+
+        this._userAuthDataService
+            .getAll(query)
+            .then(async (usersData: Array<UserAuthData>) => {
+                if (!usersData.length) {
+                    this._logger.warn('There are no users with Fitbit access to perform synchronization...')
+                    return
+                }
+                // Create jobs and add them to queue to process later
+                usersData.forEach(item => this.syncQueue!.add(item))
+            })
+            .catch(err => {
+                this._logger.error(`An error occurred while trying to retrieve data for Fitbit sync. ${err.message}`)
+            })
     }
 }
